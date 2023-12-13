b648dded9d0ad0b5afc97d29aaac531324f5ea26
Rob Faust <robert.faust@labelzoom.net>
2023-12-13 11:39:18 -0600

    Day 3

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text.RegularExpressions;
 
-Console.WriteLine("Hello, World!");
+var tasks = new List<Task<string>>();
 
+///
 /// Day 1 - Part 1
+/// 
+tasks.Add(Task.Run(() =>
 {
     int sum = 0;
     foreach (string line in File.ReadAllLines(Path.Combine("PuzzleInput", "day01.txt")))
@@ -26,10 +29,13 @@ Console.WriteLine("Hello, World!");
         }
         sum += 10 * (int)char.GetNumericValue(firstDigit ?? '0') + (int)char.GetNumericValue(lastDigit ?? '0');
     }
-    Console.WriteLine($"Day 1 - Part 1: {sum}");
-}
+    return $"Day 1 - Part 1: {sum}";
+}));
 
+///
 /// Day 1 - Part 2
+/// 
+tasks.Add(Task.Run(() =>
 {
     int sum = 0;
     foreach (string line in File.ReadAllLines(Path.Combine("PuzzleInput", "day01.txt")))
@@ -109,10 +115,13 @@ Console.WriteLine("Hello, World!");
         }
         sum += 10 * (int)char.GetNumericValue(firstDigit ?? '0') + (int)char.GetNumericValue(lastDigit ?? '0');
     }
-    Console.WriteLine($"Day 1 - Part 2: {sum}");
-}
+    return $"Day 1 - Part 2: {sum}";
+}));
 
+///
 /// Day 2 - Part 1
+/// 
+tasks.Add(Task.Run(() =>
 {
     int sum = 0;
     foreach (string line in File.ReadAllLines(Path.Combine("PuzzleInput", "day02.txt")))
@@ -149,10 +158,13 @@ Console.WriteLine("Hello, World!");
             sum += dayNumber;
         }
     }
-    Console.WriteLine($"Day 2 - Part 1: {sum}");
-}
+    return $"Day 2 - Part 1: {sum}";
+}));
 
+///
 /// Day 2 - Part 2
+/// 
+tasks.Add(Task.Run(() =>
 {
     int sum = 0;
     foreach (string line in File.ReadAllLines(Path.Combine("PuzzleInput", "day02.txt")))
@@ -186,5 +198,70 @@ Console.WriteLine("Hello, World!");
         }
         sum += redCount * greenCount * blueCount;
     }
-    Console.WriteLine($"Day 2 - Part 2: {sum}");
+    return $"Day 2 - Part 2: {sum}";
+}));
+
+///
+/// Day 3 - Part 1
+/// 
+tasks.Add(Task.Run(() =>
+{
+    Func<char, bool> isSymbol = (char character) => !char.IsDigit(character) && character != '.';
+    Func<string[], int, int, int, bool> hasAdjacentChar = (string[] lines, int lineNumber, int startChar, int endChar) =>
+    {
+        if (lineNumber > 0)
+        {
+            for (int col = Math.Max(0, startChar - 1); col <= Math.Min(endChar + 1, lines.Length - 1); col++)
+            {
+                if (isSymbol(lines[lineNumber - 1][col])) return true;
+            }
+        }
+        if (startChar > 0 && isSymbol(lines[lineNumber][startChar - 1])) return true;
+        if (endChar < lines[lineNumber].Length - 1 && isSymbol(lines[lineNumber][endChar + 1])) return true;
+        if (lineNumber < lines.Length - 1)
+        {
+            for (int col = Math.Max(0, startChar - 1); col <= Math.Min(endChar + 1, lines.Length - 1); col++)
+            {
+                if (isSymbol(lines[lineNumber + 1][col])) return true;
+            }
+        }
+        return false;
+    };
+    int sum = 0;
+    string[] lines = File.ReadAllLines(Path.Combine("PuzzleInput", "day03.txt"));
+    for (int lineNumber = 0; lineNumber < lines.Length; lineNumber++)
+    {
+        string line = lines[lineNumber];
+        for (int startChar = 0; startChar < line.Length; startChar++)
+        {
+            if (char.IsDigit(line[startChar]))
+            {
+                int endChar = startChar + 1;
+                for (; endChar < line.Length; endChar++)
+                {
+                    if (!char.IsDigit(line[endChar]))
+                    {
+                        break;
+                    }
+                }
+                int numberLength = endChar - startChar;
+                if (hasAdjacentChar(lines, lineNumber, startChar, endChar - 1))
+                {
+                    int number = int.Parse(line.Substring(startChar, numberLength));
+                    sum += number;
+                }
+                startChar += numberLength;
+            }
+        }
+    }
+    return $"Day 3 - Part 1: {sum}";
+}));
+
+Task.WaitAll(tasks.ToArray());
+
+// Print output of each task
+foreach (var task in tasks)
+{
+    var result = await task;
+    Console.WriteLine(result);
 }
